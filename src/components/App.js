f8aeb5fd323a8a9af962759e5e8d71858b808c23
Rollno45 <60298503+Rollno45@users.noreply.github.com>
2022-02-13 00:35:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ class App extends React.Component{
   render(){
     return(
       <>
-      <h1>My Project</h1>
-      <h2>Its a secret</h2>
+       <h1 data-ns-test="project-name">My Project</h1>
+      <h2 data-ns-test="project-description">Its a secret</h2>
       </>
     );
   }
