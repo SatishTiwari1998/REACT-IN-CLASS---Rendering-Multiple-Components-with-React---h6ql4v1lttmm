2d90fff7f60911ca4a24414d603127352c771b27
Rollno45 <60298503+Rollno45@users.noreply.github.com>
2022-02-13 00:32:42 +0530

    updatiing 1.0
    meri marji..........mai to update karungaa

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
+
+import React from "react";
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      <h1>My Project</h1>
+      <h2>Its a secret</h2>
+      </>
+    );
+  }
 }
-
 
 export default App;
