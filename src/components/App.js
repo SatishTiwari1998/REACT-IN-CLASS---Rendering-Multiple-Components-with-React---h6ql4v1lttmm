a95fe5919a848a9c373cb1a47e55ef813c5131b8
Rollno45 <60298503+Rollno45@users.noreply.github.com>
2022-02-13 16:01:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 
 import React from "react";
-import '../styles/App.css';
 
 class App extends React.Component{
   render(){
     return(
       <>
        <h1 data-ns-test="project-name">My Project</h1>
-      <h2 data-ns-test="project-description">Its a secret</h2>
+        <h2 data-ns-test="project-description">Its a secret</h2>
       </>
     );
   }
